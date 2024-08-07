6e777bdb0051348b942ad0289efa1b43beddf373
LRWyatt801 <loganwyatt1995@gmail.com>
2024-08-06 23:13:35 -0500

    Added filters

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+  const amenities = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const ID = $(this).data('data-id');
+    const NAME = $(this).data('data-name');
+
+    if ($(this).is(':checked')) {
+      amenities[ID] = NAME;
+    } else {
+      delete amenities[ID];
+    }
+
+    const amenitiesNum = Object.values(amenities);
+    $('div.amenities h4').text(amenitiesNum.join(', '));
+  });
+
+  // make api get request to check status on DOM ready
+  $.get('http://localhost:5001/api/v1/status/', (response) => {
+    // check response
+    if (response.status === "OK") {
+      // if response is 'OK' then add class to div id=api_status
+      $('div#api_status').addClass('available');
+    } else {
+      // if response not 'OK' then remove class from div id=api_status
+      $('div#api_status').removeClass('available');
+    }
+  }).fail(() => {
+    // if fail to get response remove class from div id=api_status
+    $('div#api_status').removeClass('available');
+  });
+
+  function searchPlaces(filters = {}) {
+    // make ajax POST request for places
+    $.ajax({
+      url: 'http://localhost:5001/api/v1/places_search/',
+      type: 'POST',
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(filters),
+      success: function (data) {
+        $('SECTION.places').empty(); // clear existing search
+        data.forEach(place => {
+          $('SECTION.places').append(
+            `<article>
+                <div class="title_box">
+                    <h2>${place.name}</h2>
+                    <div class="price_by_night">\$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                    <div class="max_guest">${place.max_guest} Guests</div>
+                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                </div>
+                <div class="description">
+                    ${place.description}
+                </div>
+            </article>`);
+        });
+      },
+    });
+  };
+
+  searchPlaces();
+
+  $('#searchButton').click(function () {
+    const fileters = { amenities: Object.keys(amenities) };
+    searchPlaces(fileters);
+    console.log('test');
+  });
+});
